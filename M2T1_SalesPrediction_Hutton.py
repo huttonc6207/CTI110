b198078a94cc60dcc1f4cee8be5b5494c62ac63e
huttonc6207 <31512214+huttonc6207@users.noreply.github.com>
2017-08-31 10:10:43 -0400

    Add files via upload
    M2T1

---
+++ M2T1_SalesPrediction_Hutton.py
@@ -0,0 +1,14 @@
+#CTI-110
+#M2T1 - Sales Prediction
+#Charles Hutton
+#8/28
+##
+
+# Total Sales
+totalSales = float(input ('Enter the projected sales: '))
+
+# Calculate the profit as 23 percent of total sales.
+profit = totalSales * 0.23
+
+#Display the profit.
+print('The profit is $', format(profit, ',.2f'))
